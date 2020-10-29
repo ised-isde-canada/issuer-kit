6a6dd17c7018ab57e84e7a883a700bb44cabd6cc
Nanda Harpreet <nanda.harpreet@canada.ca>
2020-10-29 14:30:50 -0400

    more logging

--- api/src/mongodb.ts
+++ api/src/mongodb.ts
@@ -4,7 +4,7 @@ import { Application } from "./declarations";
 export default function (app: Application) {
   const settings = app.get("mongodb");
   const x = "doing some logging";
-  console.log(x,${settings.host});
+  console.log(x,settings.host);
   const connection = `mongodb://${settings.user}:${settings.password}@${settings.host}:${settings.port}/${settings.db}`;
   const database = connection.substr(connection.lastIndexOf("/") + 1);
   const mongoClient = MongoClient.connect(connection, {
