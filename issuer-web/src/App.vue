b7ba1cd615d2507b95de9c8915028900f265b76f
Wendy <wendy.desnoyers@canada.ca>
2020-12-11 15:32:08 -0500

    Removing vue header

--- issuer-web/src/App.vue
+++ issuer-web/src/App.vue
@@ -37,26 +37,10 @@
 
 <template>
   <v-app>
-    <Header />
-    
+
     <v-main>
       <router-view></router-view>
     </v-main>
 
   </v-app>
 </template>
-
-
-<script>
-import Header from "@/components/Header";
-
-export default {
-  name: "app",
-  components: {
-    Header
-  },
-  data() {
-    return {};
-  }
-};
-</script>
--- issuer-web/src/components/Header.vue
+++ issuer-web/src/components/Header.vue
@@ -1,46 +1,23 @@
 <template>
-<div id="def-top">
-  <!-- Write closure fall-back static file -->
-  <!-- /ROOT/app/cls/WET/gcweb/v4_0_30/cdts/static/appTop-en.html -->
-  <ul id="wb-tphp">
-       <li class="wb-slc"><a class="wb-sl" href="#wb-cont">Skip to main content</a></li>
-       <li class="wb-slc visible-sm visible-md visible-lg"><a class="wb-sl" href="#wb-info">Skip to "About this Web application"</a></li>
-  </ul>
-<div class="gc-app">
-  <div id="wb-bnr" class="container">
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <h1>{{ issuerName }}</h1>
+    </div>
 
-    <div class="row">
-      <div class="brand col-xs-5 col-md-4">
-        <a href="https://www.canada.ca/en.html">
-          <img src="https://www.canada.ca/etc/designs/canada/cdts/gcweb/v4_0_30/assets/sig-blk-en.svg" alt="" />
-          <span class="wb-inv"> Government of Canada / <span lang="fr">Gouvernement du Canada</span></span>
-        </a>
-      </div>
+    <div class="d-flex align-center">
+      <h2>This page must call the Corporations Canada Service.</h2>
     </div>
-  </div>
-  <v-app-bar app light flat>
-  <div class="app-bar">
-    <div class="container row">
-      <div class="row">
-        <!-- If you want to use the settings or sign in/out buttons then add col-sm-7 -->
-        <section class="col-xs-6">
-          <!-- Add in your application name and URL below -->
-          <h2 class="wb-inv">Corporations digital credential issuing app</h2>
-          <a class="app-name" href="#">Corporations digital credential issuing app</a>
-        </section>
-        <div class="col-xs-6" v-if="oidcUser">
-          Signed in as {{ oidcUser.given_name }} {{ oidcUser.family_name }}
-          <v-btn @click="signOutOidc" text>
-            <span class="mr-2">Leave</span>
-            <v-icon>mdi-logout</v-icon>
-          </v-btn>
-        </div>
-      </div>
+
+    <v-spacer></v-spacer>
+
+    <div v-if="oidcUser">
+      Signed in as {{ oidcUser.given_name }} {{ oidcUser.family_name }}
+      <v-btn @click="signOutOidc" text>
+        <span class="mr-2">Leave</span>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
     </div>
-  </div>
   </v-app-bar>
-</div>
-</div>
 </template>
 
 <script lang="ts">
