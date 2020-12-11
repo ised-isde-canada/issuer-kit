81ac8b2752f0d7dc07b2ef0f3c27be0720235d7c
Wendy <wendy.desnoyers@canada.ca>
2020-12-11 12:48:48 -0500

    test

--- issuer-web/src/App.vue
+++ issuer-web/src/App.vue
@@ -1,28 +1,11 @@
 <template>
   <v-app>
-    <Header />
+
 
     <v-main>
       <router-view></router-view>
     </v-main>
 
-    <Footer />
+
   </v-app>
 </template>
-
-
-<script>
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-
-export default {
-  name: "app",
-  components: {
-    Header,
-    Footer
-  },
-  data() {
-    return {};
-  }
-};
-</script>
