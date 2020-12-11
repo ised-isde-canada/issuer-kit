156e76acf28f9449b9886059e5b3310f2a237b1d
Wendy <wendy.desnoyers@canada.ca>
2020-12-11 10:15:10 -0500

    Update to template

--- issuer-web/src/components/Header.vue
+++ issuer-web/src/components/Header.vue
@@ -1,34 +1,61 @@
-<style lang="scss">
-.v-toolbar__content {
-  border-bottom: 2px solid var(--v-secondary-base);
-}
-
-.issuer-logo {
-  height: 50px;
-  width: 50px;
-}
-</style>
-
 <template>
-  <v-app-bar app color="primary" dark>
-    <div class="d-flex align-center">
-      <h1>{{ issuerName }}</h1>
-    </div>
+<div id="def-top">
+  <!-- Write closure fall-back static file -->
+  <!-- /ROOT/app/cls/WET/gcweb/v4_0_30/cdts/static/appTop-en.html -->
+  <ul id="wb-tphp">
+       <li class="wb-slc"><a class="wb-sl" href="#wb-cont">Skip to main content</a></li>
+       <li class="wb-slc visible-sm visible-md visible-lg"><a class="wb-sl" href="#wb-info">Skip to "About this Web application"</a></li>
+  </ul>
+<header class="gc-app">
+  <div id="wb-bnr" class="container">
 
-    <div class="d-flex align-center">
-      <h2>This page must call the Corporations Canada Service.</h2>
+    <div class="row">
+      <div class="brand col-xs-5 col-md-4">
+        <a href="https://www.canada.ca/en.html">
+          <img src="https://www.canada.ca/etc/designs/canada/cdts/gcweb/v4_0_30/assets/sig-blk-en.svg" alt="" />
+          <span class="wb-inv"> Government of Canada / <span lang="fr">Gouvernement du Canada</span></span>
+        </a>
+      </div>
     </div>
+  </div>
 
-    <v-spacer></v-spacer>
+  <div class="app-bar">
+    <div class="container">
+      <div class="row">
+        <!-- If you want to use the settings or sign in/out buttons then add col-sm-7 -->
+        <section class="col-xs-12">
+          <!-- Add in your application name and URL below -->
+          <h2 class="wb-inv">{{ issuerName }}</h2>
+          <a class="app-name" href="#">{{ issuerName }}</a>
 
-    <div v-if="oidcUser">
-      Signed in as {{ oidcUser.given_name }} {{ oidcUser.family_name }}
-      <v-btn @click="signOutOidc" text>
-        <span class="mr-2">Leave</span>
-        <v-icon>mdi-logout</v-icon>
-      </v-btn>
+
+          <div v-if="oidcUser">
+            Signed in as {{ oidcUser.given_name }} {{ oidcUser.family_name }}
+            <v-btn @click="signOutOidc" text>
+              <span class="mr-2">Leave</span>
+              <v-icon>mdi-logout</v-icon>
+            </v-btn>
+          </div>
+
+        </section>
+      </div>
     </div>
-  </v-app-bar>
+  </div>
+
+
+</header>
+</div>
+<!-- Write closure template -->
+<script>
+  var defTop = document.getElementById("def-top");
+  defTop.outerHTML = wet.builder.appTop({
+
+    "appName": [{"text": "Corporations digital credential issuing app", "href": "#"}],
+    "lngLinks": [{"lang": "fr",	"href": "french.html", "text": "Français"}]
+  });
+</script>
+<main property="mainContentOfPage" class="container">
+
 </template>
 
 <script lang="ts">
