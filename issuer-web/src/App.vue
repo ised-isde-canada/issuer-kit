2b51a1616a7dfedf1053642873b2f034c7823a3c
Wendy <wendy.desnoyers@canada.ca>
2020-12-11 13:42:15 -0500

    Changes to template

--- issuer-web/src/App.vue
+++ issuer-web/src/App.vue
@@ -2,6 +2,8 @@
 .container {
  padding-right:15px;
  padding-left:15px;
+ padding-top:0px;
+ padding-bottom:0px;
  margin-right:auto;
  margin-left:auto
 }
@@ -28,15 +30,33 @@
   width:1170px
  }
 }
+.v-application--wrap {
+  min-height: 100px;
+}
 </style>
+
 <template>
   <v-app>
-
+    <Header />
 
     <v-main>
       <router-view></router-view>
     </v-main>
 
-
   </v-app>
 </template>
+
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  name: "app",
+  components: {
+    Header
+  },
+  data() {
+    return {};
+  }
+};
+</script>
