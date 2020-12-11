e3d2eb2772a6e7bbe6373d891834fa5b5ef42ae4
Wendy <wendy.desnoyers@canada.ca>
2020-12-11 12:49:42 -0500

    test

--- issuer-web/src/App.vue
+++ issuer-web/src/App.vue
@@ -1,3 +1,34 @@
+<style>
+.container {
+ padding-right:15px;
+ padding-left:15px;
+ margin-right:auto;
+ margin-left:auto
+}
+.container:after,
+.container:before {
+ display:table;
+ content:" "
+}
+.container:after {
+ clear:both
+}
+@media (min-width:768px) {
+ .container {
+  width:750px
+ }
+}
+@media (min-width:992px) {
+ .container {
+  width:970px
+ }
+}
+@media (min-width:1200px) {
+ .container {
+  width:1170px
+ }
+}
+</style>
 <template>
   <v-app>
 
