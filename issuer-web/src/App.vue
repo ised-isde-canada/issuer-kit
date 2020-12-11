90e96f78c7b978cd45752d42e44e15667721ff07
Wendy <wendy.desnoyers@canada.ca>
2020-12-11 12:18:11 -0500

    Template again

--- issuer-web/src/App.vue
+++ issuer-web/src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <v-app>
-    <Header />
 
     <v-main>
       <router-view></router-view>
     </v-main>
 
-    <Footer />
   </v-app>
 </template>
+
 
 <script>
 import Header from "@/components/Header";
