37fc69cbf2d4464b1e87d6d670d791f7dd345acb
Wendy <wendy.desnoyers@canada.ca>
2020-12-15 15:56:08 -0500

    giving up

--- issuer-web/src/App.vue
+++ issuer-web/src/App.vue
@@ -37,48 +37,8 @@
 
 <template>
   <v-app>
-
-  <v-app-bar app color="primary" dark>
-    <v-spacer></v-spacer>
-
-    <div v-if="oidcUser">
-      Signed in as {{ oidcUser.given_name }} {{ oidcUser.family_name }}
-      <v-btn @click="signOutOidc" text>
-        <span class="mr-2">Leave</span>
-        <v-icon>mdi-logout</v-icon>
-      </v-btn>
-    </div>
-  </v-app-bar>
-
-
     <v-main>
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
-
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-import { mapActions, mapGetters } from "vuex";
-import { Configuration } from "../models/appConfig";
-
-@Component({
-  computed: {
-    ...mapGetters("oidcStore", ["oidcUser"])
-  },
-  methods: {
-    ...mapActions("oidcStore", ["signOutOidc"])
-  }
-})
-export default class Header extends Vue {
-  @Prop() logoutUrl!: string;
-  private issuerName = "";
-
-  created() {
-    const appConfig = this.$store.getters[
-      "configuration/getConfiguration"
-    ] as Configuration;
-    this.issuerName = appConfig.app.issuer.name;
-  }
-}
-</script>
