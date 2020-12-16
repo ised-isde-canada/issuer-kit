f589f13e58378177a279d6c4ae9162a735a70e6f
Wendy <wendy.desnoyers@canada.ca>
2020-12-16 17:01:22 -0500

    fix

--- issuer-web/src/App.vue
+++ issuer-web/src/App.vue
@@ -38,23 +38,7 @@
 <template>
   <v-app>
     <v-main>
-      <Header />
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
-
-
-<script>
-import Header from "@/components/Header";
-
-export default {
-  name: "app",
-  components: {
-    Header
-  },
-  data() {
-    return {};
-  }
-};
-</script>
