728ffc038dc42eeed1b3effa9eb31e7f53f05d74
Wendy <wendy.desnoyers@canada.ca>
2020-12-17 11:36:06 -0500

    Test header

--- issuer-web/src/App.vue
+++ issuer-web/src/App.vue
@@ -37,8 +37,22 @@
 
 <template>
   <v-app>
+    <Header />
     <v-main>
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
+
+<script>
+import Header from "@/components/Header";
+export default {
+  name: "app",
+  components: {
+    Header
+  },
+  data() {
+    return {};
+  }
+};
+</script>
