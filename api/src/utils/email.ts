776e2272820950c56637ce6907ff8affbea9779c
Nanda Harpreet <nanda.harpreet@canada.ca>
2020-12-31 15:01:17 -0500

    adding more debugging to send invites

--- api/src/utils/email.ts
+++ api/src/utils/email.ts
@@ -13,7 +13,7 @@ const inject = (str: string, obj: { [index: string]: any }) =>
 
 export async function sendEmail(context: HookContext) {
 
-    logger.debug(`This is the data [${context.data.data}]`);
+    console.log(`This is the data  ${JSON.stringify(context.data)}`);
   const settings = context.app.get("emailSettings");
   const inviteUrl = `${context.app.get("publicSite").url}/?invite_token=${
     context.result.token
