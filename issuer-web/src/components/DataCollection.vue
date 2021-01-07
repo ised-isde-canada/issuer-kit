d332542e8ac4b890916669542346efb4140f7f3b
Nanda Harpreet <nanda.harpreet@canada.ca>
2021-01-07 13:54:19 -0500

    Changes for claims-connect

--- issuer-web/src/components/DataCollection.vue
+++ issuer-web/src/components/DataCollection.vue
@@ -63,8 +63,8 @@ export default class DataCollection extends Vue {
       this.$store.commit("credential/updateClaims", credentialClaims);
 
       // Go to next page on successful completion
-        this.$router.push({ path: "confirm-data" });
-        //this.$router.push({ path: "connect" });
+        //this.$router.push({ path: "confirm-data" });
+        this.$router.push({ path: "connect" });
       //'connect'
     });
 
