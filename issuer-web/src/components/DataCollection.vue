e5cdf76cf1b53887883541c88da9dfe8129d1d54
Nanda Harpreet <nanda.harpreet@canada.ca>
2021-01-28 17:13:45 -0500

    checking for VR in data entry view

--- issuer-web/src/components/DataCollection.vue
+++ issuer-web/src/components/DataCollection.vue
@@ -103,8 +103,8 @@ export default class DataCollection extends Vue {
   private valuesFromInvitation(): void {
     const invitation = this.$store.getters[
       "invitation/getInvitation"
-    ] as Invitation;
-    if(invitation.data.hasOwnProperty("relationship")){
+    ] as Invitation;{
+    if(Object.prototype.hasOwnProperty.call(invitation.data, "relationship")){
       this.entity = "Relationship";
     }
     else{
