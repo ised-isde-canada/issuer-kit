7dc85078922540b2269a3b315a8143eadec1e82d
Nanda Harpreet <nanda.harpreet@canada.ca>
2021-03-16 21:13:01 -0400

    Changing API limits for callbacks

--- api/src/app.ts
+++ api/src/app.ts
@@ -26,9 +26,13 @@ app.configure(configuration());
 app.use(helmet());
 app.use(cors());
 app.use(compress());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({limit: '10mb'}));
 app.use(favicon(path.join(app.get("public"), "favicon.ico")));
+
+
 // Host the public folder
 app.use("/", express.static(app.get("public")));
 
